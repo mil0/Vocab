4e47089e542f7db8134a27d2ca090a8eda11c1ed
milo <mkock@umich.edu>
2013-10-29 17:56:52 -0400

    Generated vocab_app ('rails new vocab_app')

---
+++ vocab_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VocabApp::Application.config.secret_token = 'f7f00298b78cb8801dfa6170afa4821e70b866c4ee526fed940b4a4fb7e97284435f696cc9f376dc289c84a1aebcd980fea05074a777decbe930dc62ebb35ee2'
